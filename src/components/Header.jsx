9a4963582376b1fe66ccc6c272932db45160c814
KJD07 <100962576+KJD07@users.noreply.github.com>
2022-08-09 01:56:53 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import AutoStoriesIcon from "@material-ui/icons/AutoStories";
+import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <LocalLibraryIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
